13b78e4cb7a3aa588f55d438740532ee7a357257
bhaveshjain144 <bhaveshjain1520.com>
2024-02-22 01:42:08 +0530

    config

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   esModule: true,
-  fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'ico', 'svg', 'pdf'], // Add 'pdf' to the list of file extensions
+  fileExtensions: ['Png', 'PNG', 'jpg', 'jpeg', 'png', 'gif', 'ico', 'svg', 'pdf'], // Add 'pdf' to the list of file extensions
 });
